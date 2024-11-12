d1704e2a56245bff1c1d273a9ddcee69d8f18916
rampmail29 <rampmail29@gmail.com>
2024-11-12 06:40:05 -0500

    [change] app.js CORS

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 import express from "express";
 import trabajadoresRoutes from "./src/routes/trabajadores.routes.js";
-import cors from 'cors';
-import pkg from 'express-validator';
+import cors from "cors";
+import pkg from "express-validator";
 import { formatoRta } from "./src/scripts/formatoRta.js";
 //const { body, query, matchedData, validationResult, ExpressValidator, CustomValidationChain, ValidationChain } = pkg;
 
 const app = express();
 
-app.use(cors({ origin: 'https://ttproject.netlify.app/' }));
+app.use(cors()); //"conversationId":"9b1f8c79-d1bf-4219-9149-b677adc28856","source":"instruct"}
 
 app.use(express.json());
 
-app.use('/api', trabajadoresRoutes);
+app.use("/api", trabajadoresRoutes);
 
 ////////////////////////////////////////////////////////////////////////////////////////
 app.use((req, res, next) => {
-    console.log("peticion a ruta no encontrada...");
-    res.status(404).json(formatoRta("", "endpoint no encontrado...."));
+  console.log("peticion a ruta no encontrada...");
+  res.status(404).json(formatoRta("", "endpoint no encontrado...."));
 });
 
 export default app;
